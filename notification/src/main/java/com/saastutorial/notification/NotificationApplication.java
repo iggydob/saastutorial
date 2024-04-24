8c771978a735a9ae2553806d2b812738344ddd46
iggydob <138367360+iggydob@users.noreply.github.com>
2024-04-24 10:39:30 +0300

    Refactoring Feign Clients for CustomerApplication.java, NotificationApplication.java and FraudApplication.java

--- notification/src/main/java/com/saastutorial/notification/NotificationApplication.java
+++ notification/src/main/java/com/saastutorial/notification/NotificationApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.context.annotation.PropertySources;
 
 @SpringBootApplication(
         scanBasePackages = {
@@ -13,8 +15,11 @@ import org.springframework.context.annotation.Bean;
                 "com.saastutorial.amqp",
         }
 )
-
 @EnableDiscoveryClient
+@PropertySources(
+        @PropertySource("classpath:clients-${spring.profiles.active}.properties")
+)
+
 public class NotificationApplication {
     public static void main(String[] args) {
         SpringApplication.run(NotificationApplication.class, args);
